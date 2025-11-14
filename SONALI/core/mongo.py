bf717ba4bf909fc14244e81b652da17dbb536898
vipin-kumar735 <patelharimohan31@gmail.com>
2025-11-14 18:08:47 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- SONALI/core/mongo.py
+++ SONALI/core/mongo.py
@@ -6,28 +6,11 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://PurviBots:PublicMongo@cluster0.gy2adez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-if config.MONGO_DB_URI is None:
-    LOGGER(__name__).warning(
-        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝐓𝐞𝐦𝐩 𝐃atabase"
-    )
-    temp_client = Client(
-        "SONALI",
-        bot_token=config.BOT_TOKEN,
-        api_id=config.API_ID,
-        api_hash=config.API_HASH,
-    )
-    temp_client.start()
-    info = temp_client.get_me()
-    username = info.username
-    temp_client.stop()
-    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
-    _mongo_sync_ = MongoClient(TEMP_MONGODB)
-    mongodb = _mongo_async_[username]
-    pymongodb = _mongo_sync_[username]
-else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+LOGGER(__name__).info("Connecting to your Mongo Database...")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Yukki
-    pymongodb = _mongo_sync_.Yukki
+    LOGGER(__name__).info("Connected to your Mongo Database.")
+except:
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    exit()
